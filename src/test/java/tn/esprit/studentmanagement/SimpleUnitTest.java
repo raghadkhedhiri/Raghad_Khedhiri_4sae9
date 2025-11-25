b58a7e68aadf1f4253f9d63687b4b5ee2fb54829
Raghad Khedhiri <123326129+raghadkhedhiri@users.noreply.github.com>
2025-11-25 21:56:47 +0100

    Add simple unit test for CI

---
+++ src/test/java/tn/esprit/studentmanagement/SimpleUnitTest.java
@@ -0,0 +1,17 @@
+package tn.esprit.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleUnitTest {
+
+    @Test
+    void additionShouldWork() {
+        int a = 2;
+        int b = 3;
+        int result = a + b;
+
+        assertEquals(5, result);
+    }
+}
