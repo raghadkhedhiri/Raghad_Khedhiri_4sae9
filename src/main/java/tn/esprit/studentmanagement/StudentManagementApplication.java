1898bbf05ce46ad4f3feb62b88b8b68aa4f85057
Raghad Khedhiri <123326129+raghadkhedhiri@users.noreply.github.com>
2025-12-17 16:00:15 +0100

    Fix Spring startup

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -2,12 +2,15 @@ package tn.esprit.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "tn.esprit.studentmanagement")
+@EnableJpaRepositories(basePackages = "tn.esprit.studentmanagement.repositories")
+@EntityScan(basePackages = "tn.esprit.studentmanagement.entities")
 public class StudentManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
     }
-
 }
