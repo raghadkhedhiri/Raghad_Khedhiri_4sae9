3e606f46d6f348718d60b86b1d9dcaae41e1a753
Raghad Khedhiri <123326129+raghadkhedhiri@users.noreply.github.com>
2025-12-18 19:31:24 +0100

    Use constructor injection in DepartmentService

--- src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
@@ -1,6 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Department;
 import tn.esprit.studentmanagement.repositories.DepartmentRepository;
@@ -10,8 +9,11 @@ import java.util.List;
 @Service
 public class DepartmentService implements IDepartmentService {
 
-    @Autowired
-    DepartmentRepository departmentRepository;
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
 
     @Override
     public List<Department> getAllDepartments() {
